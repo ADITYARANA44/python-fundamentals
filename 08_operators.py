c51095a96718995f6e00e23d355e7aa2540719ae
Aditya Singh Rana <adityasinghrana62@gmail.com>
2026-02-22 15:08:25 +0530

    Added operators file

---
+++ 08_operators.py
@@ -0,0 +1,60 @@
+#Operators:-
+#Operators are symbols that perform operations on variables.
+#Types:
+
+#1 Arithmetic operators
+# a = 12
+# b = 50
+
+# print(a + b)                    #Addition
+# print(a - b)                    #Subtraction
+# print(a * b)                    #Multiplication
+# print(a / b)                    #Division
+# print(a // b)                   #Floor-division
+# print(b % a)                    #Modulus
+# print(a**b)                     #Exponentiation
+
+#2 Assignment operators
+#Assignment operators are used to assign values to variables. 
+#'=' is the assignment operator 
+
+#3 Comnpund assignment operators
+# a = 10
+
+# a = a + 10
+#print(a)
+#but there is also another way to write this operation
+# a += 10
+# print (a)
+
+# +=                                 #Add and assign
+# -=                                 #Subract and assign
+# *=                                 #Multiply and assign
+# /=                                 #divide and assign
+# //=                                #Floor division and assign 
+# %=                                 #Modulus and assign 
+# **=                                #Exponentation and assign
+
+#4 Comparison operators
+#Comparision operators are used to compare variables with each other.
+#(also comparision operators will always give's answer in bool(True/false))
+
+# a = 20
+# b = 15
+
+# print(a < b)                        #less than
+# print(a > b)                        #Greater than
+# print(a <= b)                       #Less than equal to
+# print(a >= b)                       #Greater than equal to
+# print(a == b)                       #Equal to
+# print(a != b)                       #Not equal to
+
+#5 Logical operators
+#There are basically three types of logical operators.
+#'and' , 'or' and 'not'
+#eg:
+# a = 10
+# b = 20
+# print(a < b and a != b)
+# print(a > b or b > a)
+# print(not a)
