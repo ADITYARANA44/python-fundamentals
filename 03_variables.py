2cf7328be35c5df7b66b3e5ccf68a319da3f7e7d
Aditya Singh Rana <adityasinghrana62@gmail.com>
2026-02-22 14:59:05 +0530

    Added variables file

---
+++ 03_variables.py
@@ -0,0 +1,5 @@
+#Variables in python:-
+#In python variables are used as storage to store things in python.
+#eg:
+#"Lets assume i want to store number 10"
+#number = 10     #Here number is the the variable
