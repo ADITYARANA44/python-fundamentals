846d31dc56ef1657f681f91b5c4ba8e0ee3065e0
Aditya Singh Rana <adityasinghrana62@gmail.com>
2026-02-22 15:02:41 +0530

    Added data types file

---
+++ 05_data_types.py
@@ -0,0 +1,23 @@
+#Data-types in python:-
+#Data-types are the things we store in variables.
+
+#Diffent types of Data-types are:
+#1 Numbers
+# a = 12
+# b = 55.6
+# c = 86j
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+#2 Strings
+# a = "This is a string Data-type and we can store anything in a string"
+# print(a)
+
+#3 Bool
+#Bool data-types gives us only two values True/False
+#eg:
+# a = True
+# b = False
+# print(type(a))
+# print(type(b))
