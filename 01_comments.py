29fdfaa15434f84ac67f09e3d878dfb8784e2272
Aditya Singh Rana <adityasinghrana62@gmail.com>
2026-02-22 14:44:32 +0530

    Added comments file

---
+++ 01_comments.py
@@ -0,0 +1,12 @@
+#COMMENTS IN PYTHON:-
+#Comments are something that are ignored by the python interpreter.
+#We have to use '#' for writing a comment in python.
+#eg:
+#This is a single line comment.
+
+#In python there are no multi-line comments available but we can acheive it by using 'Doc-string'.
+#eg:
+"""So this is 
+a multi line comment.
+with this we can comment multiple lines
+in one go."""
