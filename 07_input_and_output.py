1e00182f435268afadf6cde9759cb85ad54ac059
Aditya Singh Rana <adityasinghrana62@gmail.com>
2026-02-22 15:06:44 +0530

    Added input and output file

---
+++ 07_input_and_output.py
@@ -0,0 +1,15 @@
+#Input and Output:-
+
+#example of output:
+# print("So this this the example of output.")
+
+#example of input:
+# a = str(input("Please tell your name sir/madam : "))
+
+#Questions:
+#Question1
+# n = int(input("Please tell your number Sir/Madam : "))
+
+#Question2
+# age = int(input("Please enter your age : "))
+# print(f"Your age is {age}")
