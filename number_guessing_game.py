bc8af6d2447c28c2e454f40126515a1577b52d25
Aditya Singh Rana <adityasinghrana62@gmail.com>
2026-02-22 14:38:39 +0530

    Added number guessing game project

---
+++ number_guessing_game.py
@@ -0,0 +1,21 @@
+#Question4 
+#Random number guessing game
+ 
+# import random
+# num = random.randint(1,20)
+# tries = 0
+
+# while True:
+#     guess = int(input("Please guess a random number between 1 to 20 =  "))
+#     if guess == num:
+#         tries = tries + 1
+#         print(f"Bravo you guess the right nnumber!! in {tries} tries")
+#         break
+
+#     elif guess < num:
+#         tries = tries + 1
+#         print("please go a little higer")
+
+#     else:
+#         tries = tries + 1
+#         print("Please go a little lower")
