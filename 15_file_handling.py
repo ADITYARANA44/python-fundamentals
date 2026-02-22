6c0f02af52982be0473267bc4c73d26cb9deef14
Aditya Singh Rana <adityasinghrana62@gmail.com>
2026-02-22 15:20:40 +0530

    Added file handling file

---
+++ 15_file_handling.py
@@ -0,0 +1,13 @@
+# File Handling:-
+
+# p = open(r"C:\Users\ranaa\Downloads\sms-spam-detection.ipynb")
+# print(p.read())
+
+# modes;
+# w = overrites
+# a = append
+# r = read 
+
+# r = open("Spider-man.txt","w")
+# r.write("Hello this is aditya and i am writing inside this file.")
+# r.close()
